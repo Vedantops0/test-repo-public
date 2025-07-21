66450337ffd042e2fb8ae95a214db35e62942ad6
Vedant Srivastava <Vedant.Srivastava@ops0.com>
2025-07-21 22:35:11 +0530

    IaC changes

--- fb9b2808-2a73-4608-abf7-eafeeaca16a8/main.tf
+++ fb9b2808-2a73-4608-abf7-eafeeaca16a8/main.tf
@@ -1,28 +1,7 @@
-module "test-401-workflow" {
-  source = "./test-401-workflow"
+provider "aws" {
+  region = var.region
 }
 
-module "abc-vedant-new-1" {
-  source = "./abc-vedant-new-1"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "nwe-adssasa-123d"
 }
-
-module "new-workflow-vedant" {
-  source = "./new-workflow-vedant"
-}
-
-module "abc-vedant-new" {
-  source = "./abc-vedant-new"
-}
-
-module "new-workflow-github-test" {
-  source = "./new-workflow-github-test"
-}
-
-module "workflow_bvc" {
-  source = "./workflow_bvc"
-}
-
-module "workflow1" {
-  source = "./workflow1"
-}
-
