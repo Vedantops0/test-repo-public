ece099661a3e4735840e45b41d5712126806d58f
Vedant Srivastava <Vedant.Srivastava@ops0.com>
2025-07-22 09:05:31 +0530

    IaC changes

--- fb9b2808-2a73-4608-abf7-eafeeaca16a8/main.tf
+++ fb9b2808-2a73-4608-abf7-eafeeaca16a8/main.tf
@@ -1,7 +1,36 @@
-provider "aws" {
-  region = var.region
+module "vedant-mmkojio" {
+  source = "./vedant-mmkojio"
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "nwe-adssasa-123d"
+module "new-git-git-vedant" {
+  source = "./new-git-git-vedant"
 }
+
+module "test-401-workflow" {
+  source = "./test-401-workflow"
+}
+
+module "abc-vedant-new-1" {
+  source = "./abc-vedant-new-1"
+}
+
+module "new-workflow-vedant" {
+  source = "./new-workflow-vedant"
+}
+
+module "abc-vedant-new" {
+  source = "./abc-vedant-new"
+}
+
+module "new-workflow-github-test" {
+  source = "./new-workflow-github-test"
+}
+
+module "workflow_bvc" {
+  source = "./workflow_bvc"
+}
+
+module "workflow1" {
+  source = "./workflow1"
+}
+
